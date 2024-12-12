36a80bd4b28ce7ee1fc92268bf6991b6634d35f6
David Riusech <v1X3Q0@gmail.com>
2024-12-12 11:49:36 -0500

    plugin added and update

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from .genElfHeader import genElfHeader
 from .kallsym import fillkallsyms
+from .bn_raw_elf_fill.add_header_binaryninja import add_header_p
 from binaryninja import PluginCommand
 
+PluginCommand.register("addHeader", "import header", add_header_p)
 PluginCommand.register("genElfHeader", "generate elf header for vmlinux", genElfHeader)
 PluginCommand.register("fillkallsyms", "fill elf header with kallsyms if it has necessar 5", fillkallsyms)
